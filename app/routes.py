d0532a5d51e54f93de78607aaf0ebfe4a3252b24
ashmit21 <58002155+ashmit21@users.noreply.github.com>
2019-11-26 04:00:40 -0800

    Fixed Bugs
    There are no bugs now and is ready to push to stable branch,

--- app/routes.py
+++ app/routes.py
@@ -62,7 +62,8 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-
+    form_picture.save(picture_path)
+    
     return picture_fn
 
 
